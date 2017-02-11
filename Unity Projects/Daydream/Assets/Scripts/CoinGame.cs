a8157dde4351a573b8630b83fe7af50e2264d830
dangeroustuna <gaccount@126.com>
2017-02-11 09:40:26 -0800

    git ignore things, testing

---
+++ Unity Projects/Daydream/Assets/Scripts/CoinGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinGame : MonoBehaviour
+{
+	public Image Coin;
+	public Text CoinCount;
+	public bool isCoinDragging;
+	public Image[] Coins;
+	public Image CoinPile;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Coin.enabled = false;
+		isCoinDragging = false;
+	}
+
+	//	void OnMouseDown ()
+	//	{
+	//		print ("Hello");
+	//
+	//	}
+
+	void OnMouseUp ()
+	{
+		Coin.enabled = false;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			isCoinDragging = true;
+			Coin.enabled = true;
+		}
+
+		if (isCoinDragging) {
+			Coin.transform.position = Input.mousePosition;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			isCoinDragging = false;
+			Coin.enabled = false;
+		}
+	}
+}
