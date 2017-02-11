583fb0e4832793fff09bdd12d3fb393f9759c3f2
Raphael Chang Menoni <raphaelchangmenoni@raphaels-mbp.wifi.bcit.ca>
2017-02-11 03:44:08 -0800

    Added Player2D PreFab

---
+++ Unity Projects/Daydream/Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour {
+
+	public float velocity = 2f;
+
+	private Rigidbody2D rigid;
+
+	void Start() {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update() {
+		if(Input.GetKey (KeyCode.D)) {
+			rigid.velocity = new Vector2 (velocity, 0f);
+		} else if(Input.GetKey (KeyCode.A)) {
+			rigid.velocity = new Vector2 (-velocity, 0f);
+		} else if(Input.GetKey (KeyCode.W)) {
+			rigid.velocity = new Vector2 (0, velocity);
+		} else if(Input.GetKey(KeyCode.S)) {
+			rigid.velocity = new Vector2 (0, -velocity);
+		}
+	}
+}
