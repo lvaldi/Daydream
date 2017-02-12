8f99fdeafd0858dff8b3be783f071803c42b1a04
Git Gud <rhiknow55@gmail.com>
2017-02-12 14:56:11 -0800

    d3 d2 d1 canvas image

---
+++ Unity Projects/Daydream/Assets/Scripts/ImgScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImgScript : MonoBehaviour {
+
+	public GameObject Main;
+	public Camera cam;
+	private float currentTime, pastTime;
+
+	void Start () {
+		pastTime = 2;
+		currentTime = 0;
+	}
+	
+	void Update () {
+		if(currentTime < pastTime){
+			currentTime += Time.deltaTime;
+		}else{
+			Main.gameObject.SetActive(true);
+			cam.gameObject.SetActive(false);
+			Destroy(this.gameObject);
+		}
+	}
+}
--- Unity Projects/Daydream/Assets/Scripts/Main.cs
+++ Unity Projects/Daydream/Assets/Scripts/Main.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
 
 	public GameObject playerPrefab;
 	public GameObject mazePrefab;
+	private bool active;
 
 	void Start () {
-		
+	
 		Instantiate(mazePrefab, new Vector3(-13, 0.4f, 12), Quaternion.Euler(0, 90, 0));
 		Instantiate(playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
+	
 	}
 	
 	void Update () {
