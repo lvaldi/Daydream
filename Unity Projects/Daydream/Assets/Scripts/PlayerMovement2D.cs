6b1caf0a79987e237b0e8a60f42e93264f73844a
RMenoni <raphael.menoni@gmail.com>
2017-02-11 18:55:11 -0800

    2D Movement done

--- Unity Projects/Daydream/Assets/Scripts/PlayerMovement2D.cs
+++ Unity Projects/Daydream/Assets/Scripts/PlayerMovement2D.cs
@@ -6,57 +6,45 @@ public class PlayerMovement2D : MonoBehaviour {
 
 	public float velocity = 2f;
 
-	private enum Direction {
-		Front, Back, Left, Right
-	};
 	private Rigidbody2D rigid;
 	private Animator animator;
-	private Direction direction;
+	private bool isWalking;
+	private float velx;
+	private float vely;
 
-	void Start() {
+	void Awake() {
 		rigid = GetComponent<Rigidbody2D> ();
 		animator = GetComponent <Animator> ();
-		direction = Direction.Front;
+		isWalking = false;
 	}
 
 	void Update() {
-		//animator.Play ("PlayerFrontWalk");
+		/*
 		if(Input.GetKey (KeyCode.D)) {
 			rigid.velocity = new Vector2 (velocity, 0f);
-			animator.SetTrigger ("playerMoveRight");
-			direction = Direction.Right;
 		} else if(Input.GetKey (KeyCode.A)) {
 			rigid.velocity = new Vector2 (-velocity, 0f);
-			animator.SetTrigger ("playerMoveLeft");
-			direction = Direction.Left;
 		} else if(Input.GetKey (KeyCode.W)) {
 			rigid.velocity = new Vector2 (0f, velocity);
-			animator.SetTrigger ("playerMoveBack");
-			direction = Direction.Back;
 		} else if(Input.GetKey(KeyCode.S)) {
 			rigid.velocity = new Vector2 (0f, -velocity);
-			animator.SetTrigger ("playerMoveFront");
-			direction = Direction.Front;
 		} else {
 			rigid.velocity = new Vector2 (0f, 0f);
-			SetIdleDirection ();
+		} */
+
+		float input_x = Input.GetAxisRaw ("Horizontal");
+		float input_y = Input.GetAxisRaw ("Vertical");
+
+
+		isWalking = (Mathf.Abs (input_x) + Mathf.Abs (input_y)) > 0;
+		animator.SetBool ("isWalking", isWalking);
+		if (isWalking) {
+			Debug.Log ("isWalking is true!!!");
+			animator.SetFloat ("x", input_x);
+			animator.SetFloat ("y", input_y);
+
+			transform.position += new Vector3 (input_x, input_y).normalized * velocity * Time.deltaTime;
 		}
 	}
 
-	private void SetIdleDirection() {
-		switch(direction) {
-		case Direction.Right:
-			animator.SetTrigger ("playerTurnRight");
-			break;
-		case Direction.Left:
-			animator.SetTrigger ("playerTurnLeft");
-			break;
-		case Direction.Back:
-			animator.SetTrigger ("playerTurnBack");
-			break;
-		default:
-			animator.SetTrigger ("playerTurnFront");
-			break;
-		}
-	}
 }
