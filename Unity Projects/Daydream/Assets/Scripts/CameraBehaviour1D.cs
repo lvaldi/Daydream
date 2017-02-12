016061301e25175e2f2cdf1b734e2e53311a9e72
Git Gud <rhiknow55@gmail.com>
2017-02-12 15:40:50 -0800

    Added transitions

--- Unity Projects/Daydream/Assets/Scripts/CameraBehaviour1D.cs
+++ Unity Projects/Daydream/Assets/Scripts/CameraBehaviour1D.cs
@@ -7,12 +7,14 @@ public class CameraBehaviour1D : MonoBehaviour {
 	public GameObject player;
 
 	private Vector3 distCameraPlayer;
+	public bool active;
 
 	void Awake() {
 		distCameraPlayer = transform.position - player.transform.position;
 	}
 
 	void Update() {
+		if(!active) return;
 		transform.position = player.transform.position + distCameraPlayer;
 	}
 }
---
+++ Unity Projects/Daydream/Assets/switchScene2to1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class switchScene2to1 : MonoBehaviour {
+
+	public GameObject img;
+	private bool canGo;
+	void Start(){
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player"){
+			Debug.Log("workds");
+			img.SetActive(true);
+
+			if(canGo)
+				SceneManager.LoadScene("1D");
+		}
+	}
+}
