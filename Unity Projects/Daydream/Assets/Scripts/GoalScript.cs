85b02e08c8d56eb75e962dda050af90c96dddb77
dangeroustuna <gaccount@126.com>
2017-02-12 15:28:30 -0800

    3D scene to 2D scene Ready

--- Unity Projects/Daydream/Assets/Scripts/GoalScript.cs
+++ Unity Projects/Daydream/Assets/Scripts/GoalScript.cs
@@ -3,30 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GoalScript : MonoBehaviour {
+public class GoalScript : MonoBehaviour
+{
 
 	public Animator anime;
-	private float height=3;
-	private float speed=2f;
+	private float height = 10;
+	private float speed = .5f;
 
-	bool startRising;
-	void OnTriggerEnter(Collider other)
+	bool startRising = false;
+
+	void OnTriggerEnter (Collider other)
 	{
-		if(other.tag == "Player"){
-			anime.SetBool("isSpinning", true);
+		if (other.tag == "Player") {
 			
-			startRising =true;
-			Debug.Log("started rising");
-			Debug.Log(transform.position.y);
+			
+			startRising = true;
+			Debug.Log ("started rising");
+			Debug.Log (transform.position.y);
 		}
 	}
-	void Update()
+
+	void Update ()
 	{	
 
-		if(startRising&& transform.position.y<height){
-			transform.position+=new Vector3(0,Time.deltaTime*speed,0);
+		if (startRising && transform.position.y < height) {
+			transform.position += new Vector3 (0, Time.deltaTime * speed, 0);
 			
 		}
-		if(transform.position.y >= height) SceneManager.LoadScene("2D");
+		if (transform.position.y >= height)
+			SceneManager.LoadScene ("2D");
 	}
 }
