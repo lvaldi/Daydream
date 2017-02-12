9fb835364f0048e8622c9242f669900474ad6886
Git Gud <rhiknow55@gmail.com>
2017-02-12 09:39:12 -0800

    minor 3d edits

--- Unity Projects/Daydream/Assets/Scripts/PlayerScript3D.cs
+++ Unity Projects/Daydream/Assets/Scripts/PlayerScript3D.cs
@@ -65,7 +65,11 @@ public class PlayerScript3D : MonoBehaviour {
 	void Turn(){
 		xRot += Input.GetAxis("Mouse X") * xSens;
 		yRot -= Input.GetAxis("Mouse Y") * ySens;
-		if(yRot <= 170) Camera.main.transform.localEulerAngles = new Vector3(yRot, 0, 0);
+		if(yRot>=80) yRot = 80;
+		if(yRot<=-80) yRot = -80;
+		if(yRot <= 80 && yRot >= -80) {
+			Camera.main.transform.localEulerAngles = new Vector3(yRot, 0, 0);
+		}
 		transform.localEulerAngles = new Vector3(0, xRot, 0);
 	}
 
@@ -86,58 +90,4 @@ public class PlayerScript3D : MonoBehaviour {
 			onGround = false;
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	public GameObject cam;
-	private Rigidbody rigid;
-	private Vector3 moveToVector;
-	private float xPos, zPos;
-	private const float yPos = 0;
-	private float speed;
-	private Vector3 moveDirection;
-	private float xRot, yRot, xSens, ySens;
-
-
-	void Awake()
-	{	
-		DOTween.Init();
-		rigid = GetComponent<Rigidbody>();
-		speed = 10;
-		xSens = 5;
-		ySens = 5;
-	}
-
-    void Update() {
-		if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) transform.position += transform.right * Time.deltaTime * speed;
-		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) transform.position -= transform.right * Time.deltaTime * speed;
-		if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) transform.position += transform.forward * Time.deltaTime * speed;
-		if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) transform.position -= transform.forward * Time.deltaTime * speed;
-		moveToVector = new Vector3(xPos, yPos, zPos) + transform.forward * Time.deltaTime;
-		
-		//transform.DOMove(moveToVector, 1);
-		
-		
-		
-
-		xRot += Input.GetAxis("Mouse X") * xSens;
-		yRot -= Input.GetAxis("Mouse Y") * ySens;
-		transform.localEulerAngles = new Vector3(yRot, xRot, 0);
-		//transform.DORotate(new Vector3(yRot, xRot, 0), 1, RotateMode.Fast);
-		transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime, 0);
-    }*/
 }
